55b4db6ceecc375c94589a405e143e06bcfed3b4
Dedie Kanda <dolente@live.com>
2016-01-25 23:56:55 -0500

    Unity Files put in place. Rough scene implemented. Plane, without textures. Two capsules, without textures. One capsule with basic script for forward motion.

---
+++ OceanAndSky/Assets/characterBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (Vector3.up * Time.deltaTime * 10);
+
+	}
+}
