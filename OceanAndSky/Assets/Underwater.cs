eed52f4bc940c74f15c9bb23de116c289dac7738
Dedie Kanda <dolente@live.com>
2016-01-30 23:06:17 -0500

    Ocean assets all intact. Water prefab and Toon shaded ocean.

---
+++ OceanAndSky/Assets/Underwater.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Underwater : MonoBehaviour {
+
+	public float waterLevel = 0;
+	private bool isUnderwater;
+	private Color normalColor;
+	private Color underwaterColor;
+
+	// Use this for initialization
+	void Start () {
+		//RenderSettings.fog = true;
+		normalColor = new Color (0.5f, 0.5f, 0.5f, 0.05f);
+		underwaterColor = new Color (0.22f, 0.65f, 0.77f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isUnderwater = transform.position.y <= waterLevel;
+		if (isUnderwater) { setUnderwater(); }
+		if (!isUnderwater) { setNormal(); }
+		/*
+		if (transform.position.y <= waterLevel) 
+		{ 
+			//isUnderwater = transform.position.y < waterLevel;
+			if (isUnderwater) { setUnderwater(); }
+			if (!isUnderwater) { setNormal(); }
+		}*/
+	}
+
+	
+	void setNormal()
+	{
+		RenderSettings.fogColor = normalColor;
+		RenderSettings.fogDensity = 0.002f;
+	}
+	
+	void setUnderwater()
+	{
+		RenderSettings.fogColor = underwaterColor;
+		RenderSettings.fogDensity = 0.03f;
+	}
+}
+
---
+++ OceanAndSky/Assets/cameraMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMove : MonoBehaviour {
+	GameObject depthPlane;
+	public float defaultPos;
+
+	// Use this for initialization
+	void Start () {
+		//depthPlane = this.transform.Find ("DepthHorizon").gameObject;
+		defaultPos = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * 20);
+		//depthPlane.transform.position.y;
+		//defaultPos = transform.position.y;
+
+		if (Input.GetKeyDown(KeyCode.C)) { defaultPos = -0.25f; }
+		if (Input.GetKey(KeyCode.UpArrow)) { transform.Translate(Vector3.up * Time.deltaTime * 10); }
+		if (Input.GetKey(KeyCode.DownArrow)) { transform.Translate(Vector3.down * Time.deltaTime * 10); }
+		if (Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(Vector3.left * Time.deltaTime * 10); }
+		if (Input.GetKey(KeyCode.RightArrow)) { transform.Translate(Vector3.right * Time.deltaTime * 10); }
+	}
+}
