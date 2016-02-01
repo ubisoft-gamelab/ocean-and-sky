1010c3b1b60d40f94c180c0f8dff9ab954d37869
SachaPF <sacha.perry-fagant@mail.mcgill.ca>
2016-02-01 11:49:21 -0500

    Add missing files

---
+++ OceanAndSky/Assets/Camera_test_scripts/CameraPosition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float yMovement = transform.position.y;
+        transform.position = new Vector3((player1.transform.position.x + player2.transform.position.x) / 2, (player1.transform.position.y + player2.transform.position.y)/2, (player1.transform.position.z + player2.transform.position.z) / 2 - 15); //this is wrong but oh well
+        yMovement = yMovement - transform.position.y;
+
+        //Trying to figure out rotation
+        //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+
+
+	
+	}
+}
---
+++ OceanAndSky/Assets/Camera_test_scripts/MovePlayer.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    public KeyCode forward;
+    public KeyCode backwards;
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+    public float bounds = 7;
+    public GameObject otherPlayer;
+    public float speed = 20;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(forward))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(left))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(right))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+        }
+
+        if (Input.GetKey(backwards))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(up))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(down))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, otherPlayer.transform.position.x - bounds, otherPlayer.transform.position.x + bounds),
+            Mathf.Clamp(rb.position.y, otherPlayer.transform.position.y - bounds, otherPlayer.transform.position.y + bounds),
+            Mathf.Clamp(rb.position.z, otherPlayer.transform.position.z - bounds, otherPlayer.transform.position.z + bounds)
+        );
+
+    }
+
+}
