24fca21b28e45e289ccc72f50aa96180c182945e
pf-physics <sacha.perry-fagant@mail.mcgill.ca>
2016-03-23 14:52:29 -0400

    Added files via upload

---
+++ OceanAndSky/Assets/Scripts/Pulse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+
+    bool canGrow;
+    Vector3 originalScale;
+    // Use this for initialization
+    void Start () {
+
+        originalScale = transform.localScale;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        grow();
+    }
+
+
+    void grow()
+    {
+        transform.localScale = new Vector3(originalScale.x + Mathf.PingPong(Time.time * 0.1f, 0.3f), originalScale.y + Mathf.PingPong(Time.time * 0.1f, 0.3f), originalScale.z);
+    }
+
+}
